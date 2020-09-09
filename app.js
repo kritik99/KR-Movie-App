96d062b387e0c43322503fe83d07a0086202d68b
kritik rathore <kritikrathore99@gmail.com>
2020-09-09 21:39:18 +0530

    Add app.js and remaining files

---
+++ app.js
@@ -0,0 +1,65 @@
+require('dotenv').config();
+var express = require("express"),
+  app = express(),
+  bodyParser = require("body-parser"),
+  mongoose = require("mongoose"),
+  Movie = require("./models/movie"),
+  Comment = require("./models/comment"),
+  passport = require("passport"),
+  LocalStrategy = require("passport-local"),
+  User = require("./models/user"),
+  methodOverride = require("method-override"),
+  flash = require("connect-flash"); 
+
+// Requiring routes
+  var commentRoutes = require("./routes/comments"),
+  movieRoutes = require("./routes/movies"),
+  indexRoutes = require("./routes/index");
+
+  var url=process.env.DATABASEURL;
+  console.log(url);
+  mongoose.connect('mongodb://localhost/movies_app', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify : false
+  });
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
+app.use(flash());
+
+// PASSPORT CONFIG
+app.use(
+  require("express-session")({
+    secret: "shibas are the best dogs in the world.",
+    resave: false,
+    saveUninitialized: false
+  })
+);
+app.locals.moment = require("moment");
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next) {
+  res.locals.currentUser = req.user;
+  res.locals.error = req.flash("error");
+  res.locals.success = req.flash("success");
+  next();
+});
+
+app.use("/", indexRoutes);
+app.use("/movies", movieRoutes);
+app.use("/movies/:id/comments", commentRoutes);
+
+app.get("*", function(req, res) {
+  res.render("error");
+});
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log("listening on http://localhost:3000/");
+});
